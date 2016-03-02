4180c78046c1d378755ef4b502bcad455c5944d2
Shlomin Maksim <m.shlomin@timeweb.ru>
2016-03-02 23:04:49 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+transliteration
+=====
+
+Cyrillic transliteration module based on simple symbol substitute with considering repetition frequency in alphabet
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/transliteration_app.erl
@@ -0,0 +1,11 @@
+-module(transliteration_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    transliteration_sup:start_link().
+
+stop(_State) ->
+    ok.
