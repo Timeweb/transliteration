abe0ead03b7ecbd4762d5654ae0cc71ef9222fb6
Shlomin Maksim <m.shlomin@timeweb.ru>
2016-03-05 08:34:13 +0300

    tests refactoring

--- test/transliteration_test.erl
+++ test/transliteration_test.erl
@@ -4,41 +4,43 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("test_helper.hrl").
 
-cyr_lat_test_() ->
-  [
-    ?_assertEqual($o, transliteration:cyr_lat(<<"о">>)),
-    ?_assertEqual($a, transliteration:cyr_lat(<<"а">>)),
-    ?_assertEqual($e, transliteration:cyr_lat(<<"е">>)),
-    ?_assertEqual($i, transliteration:cyr_lat(<<"и">>)),
-    ?_assertEqual($n, transliteration:cyr_lat(<<"н">>)),
-    ?_assertEqual($t, transliteration:cyr_lat(<<"т">>)),
-    ?_assertEqual($r, transliteration:cyr_lat(<<"р">>)),
-    ?_assertEqual($s, transliteration:cyr_lat(<<"с">>)),
-    ?_assertEqual($l, transliteration:cyr_lat(<<"л">>)),
-    ?_assertEqual($v, transliteration:cyr_lat(<<"в">>)),
-    ?_assertEqual($k, transliteration:cyr_lat(<<"к">>)),
-    ?_assertEqual($p, transliteration:cyr_lat(<<"п">>)),
-    ?_assertEqual($m, transliteration:cyr_lat(<<"м">>)),
-    ?_assertEqual($u, transliteration:cyr_lat(<<"у">>)),
-    ?_assertEqual($d, transliteration:cyr_lat(<<"д">>)),
-    ?_assertEqual("ya", transliteration:cyr_lat(<<"я">>)),
-    ?_assertEqual($y, transliteration:cyr_lat(<<"ы">>)),
-    ?_assertEqual("", transliteration:cyr_lat(<<"ь">>)),
-    ?_assertEqual($z, transliteration:cyr_lat(<<"з">>)),
-    ?_assertEqual($b, transliteration:cyr_lat(<<"б">>)),
-    ?_assertEqual($g, transliteration:cyr_lat(<<"г">>)),
-    ?_assertEqual($y, transliteration:cyr_lat(<<"й">>)),
-    ?_assertEqual("ch", transliteration:cyr_lat(<<"ч">>)),
-    ?_assertEqual("yu", transliteration:cyr_lat(<<"ю">>)),
-    ?_assertEqual("kh", transliteration:cyr_lat(<<"х">>)),
-    ?_assertEqual("zh", transliteration:cyr_lat(<<"ж">>)),
-    ?_assertEqual("sh", transliteration:cyr_lat(<<"ш">>)),
-    ?_assertEqual("ts", transliteration:cyr_lat(<<"ц">>)),
-    ?_assertEqual("shch", transliteration:cyr_lat(<<"щ">>)),
-    ?_assertEqual($f, transliteration:cyr_lat(<<"ф">>)),
-    ?_assertEqual($e, transliteration:cyr_lat(<<"э">>)),
-    ?_assertEqual("", transliteration:cyr_lat(<<"ъ">>))
-  ].
+-define(RULES, [
+  {$o, <<"о">>},
+  {$a, <<"а">>},
+  {$e, <<"е">>},
+  {$i, <<"и">>},
+  {$n, <<"н">>},
+  {$t, <<"т">>},
+  {$r, <<"р">>},
+  {$s, <<"с">>},
+  {$l, <<"л">>},
+  {$v, <<"в">>},
+  {$k, <<"к">>},
+  {$p, <<"п">>},
+  {$m, <<"м">>},
+  {$u, <<"у">>},
+  {$d, <<"д">>},
+  {"ya", <<"я">>},
+  {$y, <<"ы">>},
+  {"", <<"ь">>},
+  {$z, <<"з">>},
+  {$b, <<"б">>},
+  {$g, <<"г">>},
+  {$y, <<"й">>},
+  {"ch", <<"ч">>},
+  {"yu", <<"ю">>},
+  {"kh", <<"х">>},
+  {"zh", <<"ж">>},
+  {"sh", <<"ш">>},
+  {"ts", <<"ц">>},
+  {"shch", <<"щ">>},
+  {$f, <<"ф">>},
+  {$e, <<"э">>},
+  {"", <<"ъ">>}
+]).
+
+cyr_lat_symbols_test() ->
+  lists:foreach(fun({Lat, Cyr}) -> ?_assertEqual(Lat, transliteration:cyr_lat(Cyr)) end, ?RULES).
 
 prop_cyr_lat() ->
   ?FORALL(Str, <<>>, [] =:= transliteration:cyr_lat(Str)).
